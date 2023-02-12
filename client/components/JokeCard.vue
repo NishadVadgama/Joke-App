cdd31900bba860e3b7fca6fcdd01179123808c9f
Nishad Vadgama <nishad.vadgama@gmail.com>
2023-02-12 14:18:29 -0500

    Added Nuxt boilerplate

---
+++ client/components/JokeCard.vue
@@ -0,0 +1,4 @@
+<script lang="ts" setup></script>
+<template>
+  <h1>Joke Card</h1>
+</template>
---
+++ client/layouts/default.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const title = 'Jokes 😂'
+</script>
+<template>
+  <v-app>
+    <v-app-bar fixed app>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
